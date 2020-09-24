eea2a21ddb10fad04f6d876e6b03cd280f501da1
Andrew Hancox <andrewdchancox@googlemail.com>
2020-09-24 16:26:23 +0100

    Tidy up R pre-requisites

--- lib/prereqs.R
+++ lib/prereqs.R
@@ -1,17 +1,5 @@
 install.packages("dplyr",repos = "http://cran.us.r-project.org")
 install.packages("sirt",repos = "http://cran.us.r-project.org")
 install.packages("readr",repos = "http://cran.us.r-project.org")
-
-
-
 install.packages("rlang",repos = "http://cran.us.r-project.org")
 install.packages("mvtnorm",repos = "http://cran.us.r-project.org")
-
-ERROR: dependency ‘mvtnorm’ is not available for package ‘polycor’
-* removing ‘/usr/local/lib/R/site-library/polycor’
-ERROR: dependencies ‘mvtnorm’, ‘polycor’ are not available for package ‘CDM’
-* removing ‘/usr/local/lib/R/site-library/CDM’
-ERROR: dependency ‘CDM’ is not available for package ‘TAM’
-* removing ‘/usr/local/lib/R/site-library/TAM’
-ERROR: dependencies ‘CDM’, ‘TAM’ are not available for package ‘sirt’
-* removing ‘/usr/local/lib/R/site-library/sirt’
