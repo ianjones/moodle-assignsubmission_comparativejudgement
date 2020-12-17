4686d9c91b43afb20dffc386d3ca379be3864233
Andrew Hancox <andrewdchancox@googlemail.com>
2020-12-17 16:24:41 +0000

    Fix form defaults

--- locallib.php
+++ locallib.php
@@ -59,10 +59,10 @@ class assign_submission_comparativejudgement extends assign_submission_plugin {
 
         ];
 
-        if (isset($defaults[$key])) {
+        $config = $this->get_config($key);
+
+        if ($config === false && isset($defaults[$key])) {
             $config = $defaults[$key];
-        } else {
-            $config = $this->get_config($key);
         }
 
         return $config;
